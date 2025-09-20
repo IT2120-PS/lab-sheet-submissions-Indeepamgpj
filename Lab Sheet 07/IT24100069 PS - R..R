418a7c85ffa8318ed65843cbe010cad69323d05a
Indeepamgpj <IT24100069@my.sliit.lk>
2025-09-20 23:20:29 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24100069 PS - R..R
@@ -0,0 +1,16 @@
+setwd("C:\\Users\\USER\\Desktop\\IT24100069 - PS Lab 7")
+getwd()
+
+#Exercise
+
+#1
+punif(25,min = 0, max = 40) - punif(10, min = 0, max = 40)
+
+#2
+pexp(2, rate = 1/3)
+
+#3.1
+pnorm(130, mean = 100, sd = 15)
+
+#3.2
+qnorm(0.95, mean = 100, sd = 15)
